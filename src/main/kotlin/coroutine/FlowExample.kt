c9140b4466141e734119260b541c75e7c5a10dff
zyy <zhouyueyue1@rd.netease.com>
2020-05-01 17:57:38 +0800

    update

--- src/main/kotlin/coroutine/FlowExample.kt
+++ src/main/kotlin/coroutine/FlowExample.kt
@@ -2,10 +2,10 @@ package coroutine
 
 import coroutine.FlowExample.test1
 import coroutine.FlowExample.test2
+import coroutine.FlowExample.test3
+import coroutine.FlowExample.test4
 import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.*
 import utils.MyLog
 import utils.MyLog.log
 
@@ -26,7 +26,62 @@ object FlowExample {
     fun test2(): Sequence<Int> = sequence {
         for (i in 1..3) {
             Thread.sleep(100) // 假装我们正在计算
+            log("test2 $i")
             yield(i) // 产生下一个值
+        }
+    }
+
+    suspend fun test3() {
+        coroutineScope {
+            flowOf(1, 2, 3).collect {
+
+            }
+            (1..3).asFlow().map {
+                // 处理请求
+                "$it"
+            }.collect {
+
+            }
+            (1..3).asFlow().transform { req ->
+                // 可以发射多次值
+                emit("$req")
+                emit("$req * 2")
+            }.map {
+                log("transfrom map")
+                it
+            }.catch {
+                log("catch")
+            }.flowOn(Dispatchers.IO).collect {
+                log("collect")
+            }
+            // 末端操作符
+            (1..5).asFlow().map {
+                // 处理请求
+                it
+            }.reduce { a, b ->
+                log("a = $a b = $b")
+                a + b
+            }
+        }
+    }
+
+    suspend fun test4() {
+        coroutineScope {
+            (1..4).asFlow().map {
+                log("$it")
+                it
+            }.flowOn(Dispatchers.Default)
+                    .collect {
+                        log("collect $it")
+                    }
+        }
+    }
+
+    suspend fun test5() {
+        coroutineScope {
+            test1().buffer().collect {
+
+            }
         }
     }
 }
@@ -34,7 +89,7 @@ object FlowExample {
 @ExperimentalCoroutinesApi
 @InternalCoroutinesApi
 fun main() = runBlocking {
-//    // 启动并发的协程以验证主线程并未阻塞
+    //    // 启动并发的协程以验证主线程并未阻塞
 //    launch {
 //        for (k in 1..3) {
 //            MyLog.log("I'm not blocked $k")R
@@ -44,9 +99,10 @@ fun main() = runBlocking {
 ////
 ////    MyLog.log("end")
 
-//    test1().collect { value -> log(value) }
+    test3()
 
-    val sequence = test2()
-    sequence.take(2).forEach { value -> log(value) }
-    sequence.take(3).forEach { value -> log(value) }
+//    val sequence = test2()
+//    sequence.take(2).forEach { value -> log(value) }
+//    sequence.take(3).forEach { value -> log(value) }
+    log("end")
 }
