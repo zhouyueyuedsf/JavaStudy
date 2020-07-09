6d53b2efab96c873dd26b8736c6403e1a371cfe3
joyChou <18716833534@163.com>
2020-07-09 17:42:06 +0800

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,38 +1,42 @@
+import candy.NullTestKt;
+
 import java.util.ArrayList;
 import java.util.TimeZone;
 
 public class Main {
 
     public static void main(String[] args) {
-        char[] A= {'a','b','E','C','g'};
-        int maxKey = 'Z';
-
-        ArrayList<Character> B[] =  new ArrayList[maxKey];
-//        char[] B = new char[52];
-        for(int i = 0; i < A.length; i++){
-            int Ai = 0;
-
-            if(A[i] >= 'a'){
-                int a = 'a';
-                Ai = A[i];
-                Ai = Ai - a;
-                if(B[Ai] == null){
-                    ArrayList<Character> a1 = new ArrayList<>();
-                    B[Ai] =  a1;
-                }
-                B[Ai].add(A[i]);
-            }else{
-                Ai = A[i];
-                if(B[Ai] == null){
-                    ArrayList<Character> a1 = new ArrayList<>();
-                    B[Ai] =  a1;
-                }
-                B[Ai].add(A[i]);
-            }
-
-        }
-        TimeZone timezone = TimeZone.getDefault();
-        // ETC/GMT+3
-        System.out.println("ETC~GMT+3".replace("/", "~").replace(" ", "_").replace("+", "%"));
+        NullTestKt.f1(null);
+//        char[] A= {'a','b','E','C','g'};
+//        int maxKey = 'Z';
+//
+//        ArrayList<Character> B[] =  new ArrayList[maxKey];
+////        char[] B = new char[52];
+//        for(int i = 0; i < A.length; i++){
+//            int Ai = 0;
+//
+//            if(A[i] >= 'a'){
+//                int a = 'a';
+//                Ai = A[i];
+//                Ai = Ai - a;
+//                if(B[Ai] == null){
+//                    ArrayList<Character> a1 = new ArrayList<>();
+//                    B[Ai] =  a1;
+//                }
+//                B[Ai].add(A[i]);
+//            }else{
+//                Ai = A[i];
+//                if(B[Ai] == null){
+//                    ArrayList<Character> a1 = new ArrayList<>();
+//                    B[Ai] =  a1;
+//                }
+//                B[Ai].add(A[i]);
+//            }
+//
+//        }
+//        TimeZone timezone = TimeZone.getDefault();
+//        // ETC/GMT+3
+//        System.out.println("ETC~GMT+3".replace("/", "~").replace(" ", "_").replace("+", "%"));
+//
     }
 }
--- src/main/java/innerclass/InnerClassStudy.java
+++ src/main/java/innerclass/InnerClassStudy.java
@@ -4,4 +4,15 @@ public class InnerClassStudy {
     public class InnerClassInner{
 
     }
+
+    InnerClassStudy study = new InnerClassStudy() {
+        public void f1() {}
+    };
+
+    void f2() {
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
