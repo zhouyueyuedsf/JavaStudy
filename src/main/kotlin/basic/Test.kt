7860b56e75c79d6dfd83e67f019099f1e84105de
joyChou <18716833534@163.com>
2020-09-21 12:01:21 +0800

    update

--- src/main/kotlin/basic/Test.kt
+++ src/main/kotlin/basic/Test.kt
@@ -1,9 +1,31 @@
 package basic
 
+import java.io.*
+
 /**
  * @time 2020/6/23
  * @author joy zhou
  */
+
+fun test1(): Unit {
+    var `in`: InputStream? = null
+    try {
+        // 得到输入流
+        `in` = FileInputStream("E:\\test\\a.txt")
+        //得到输出流
+        val file = File("E:\\test\\b.txt")
+        if (!file.exists()) {
+            file.createNewFile()
+        }
+        val out: OutputStream = FileOutputStream(file, true)
+        var i: Int //从输入流读取一定数量的字节，返回 0 到 255 范围内的 int 型字节值
+        while (`in`.read().also { i = it } != -1) {
+            out.write(i)
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+}
 
 fun main() {
 //    println(LangProvider.ENGLISH)
